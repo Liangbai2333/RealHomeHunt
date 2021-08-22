58008002b4f4cdeed86f8c2a2e507e03331c3a10
Liangbai2333 <admin@liangbai.site>
2021-08-22 23:26:10 +0800

    Updated

--- src/main/java/site/liangbai/realhomehunt/internal/storage/IStorage.kt
+++ src/main/java/site/liangbai/realhomehunt/internal/storage/IStorage.kt
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package site.liangbai.realhomehunt.internal.storage
 
 import site.liangbai.realhomehunt.api.residence.Residence
